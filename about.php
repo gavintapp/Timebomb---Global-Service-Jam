b92709ff9625eba17315fa7175aa174002e637f6
Gavin Tapp <gavin@solonode.com>
2012-02-26 15:29:32 +1100

    Added about page with background on the project

---
+++ about.php
@@ -0,0 +1,48 @@
+<?php include('header.php'); ?>
+
+<div role="main" id="main" >
+<h2>About Timebomb</h2>
+<p>Timebomb was created at the Canberra Global Service Jam in 2012 over the course of three days. 
+	It was inspired by the theme for 2012 of "Hidden Treasure".</p>
+
+<p>We explored a lot of ideas about what "Hidden Treasure" might be - and talked about ideas and information as a kind of treasure.
+We thought it would be interesting to find a way to hide this treasure away - but build in a way to release the treasure at a later 
+time.</p>
+
+<p>This got us thinking about lots of different ways that this could be applied - and really, there are lots of little ways in
+	 which you could use a service like this. Let's take a simple story.</p>
+<p>A child wants to hide their favourite toy so that it can't be taken by an older brother. They find a secret place, deep in the woods. 
+	They dig a hole and carefully bury the toy, stored in a box. They walk home, happy that the toy is safe.</p>
+<p>But months pass - and they forget where the toy was buried. They would have written a note, but they were worried the brother would 
+	would find it. Where is a safe place where they could record where the toy was hidden?</p>
+<p>What if when they hid the toy, they took a photo on their phone, and uploaded it to a safe place? And rather than storing the 
+	photo on their phone or computer (where their brother might find it) - they store this secret online in a safe place. The safe 
+	place sends them an email with the photo in a couple of months when the danger has passed.</p>
+<p>Now, this is a pretty trivial example - but what if the info being stored was more important than just the location of a toy? 
+	And what if you could set the release date for the secret to a time far into the future?
+	What if the secret was: </p>
+<ul>
+	<li>Information to bring down a government</li>
+	<li>A deep dark family secret, only to be revealed after you die?</li>
+	<li>The secret stash of family photos?</h1>
+	<li>Your 'insurance policy'? ie - if anything happens to me, damaging info is released to the press.</li>
+</ul>
+
+<p>Really - it could be anything that you want to release at a later time. Much like twitter - the benifit comes from the 
+wide range of possible uses when it is considered as a platform.</p>
+
+<h3>Value Proposition</h3>
+<p>With such a simple idea - the key offering here is to keep it simple, but take care of the unusual elements.</p>
+<p>Security is going to be important - so we propose to heavily encrypt all the information added to the system. This goes for both 
+	trivial and critical info - hiding needles among needles.</p>
+<p>Longevity - some secrets need to remain hidden for a long time. We propose to keep this system running for more than 50 years. If 
+	you have a secret in your youth that you want to hide until after your death - it should be able to do that.</p>
+<p>A platform for infomation - the same backend system could support a multitude of interfaces each aimed at a different use. ie the 
+	use case for a child is different than a journalist protecting sources</p>
+<p>Commercial value could be acheived through charging a fee if a secret needs to be stored for more than three months. Given the 
+	possible use for activists, journalists, whistleblowers etc - there may also be a possibility of a philanthropic backer.</p>
+
+
+</div>
+
+<? include('footer.php'); ?>
